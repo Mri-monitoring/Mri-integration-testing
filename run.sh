7c921bc5504b994962bcba58031ead6557744230
Nate Harada <nharada1@gmail.com>
2015-07-28 10:32:12 -0400

    Fixes for new command line options

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 cd Mri-app/mriapp
 # Run mri-server dispatch test
-python MriApp.py mri-server-config
+python MriApp.py --config mri-server-config
 # Run matplotlib dispatch test
-python MriApp.py matplotlib-config
+python MriApp.py --config matplotlib-config
+# Run override test
+python MriApp.py --solver_override "/home/vagrant/Files/001/solver.prototxt"
 cd -
