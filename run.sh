12f005c508fba299f5699284287c307e44ae1371
Nate Harada <nharada1@gmail.com>
2015-08-12 15:59:32 -0400

    Better integration testing

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
+sudo sed -i s/TkAgg/Agg/g /etc/matplotlibrc
 cd Mri-app/mriapp
 # Run mri-server dispatch test
 python MriApp.py --config mri-server-config
 # Run matplotlib dispatch test
 python MriApp.py --config matplotlib-config
 # Run override test
-python MriApp.py --solver_override "/home/vagrant/Files/001/solver.prototxt"
+python MriApp.py --config mri-server-config --solver_override "/home/vagrant/Files/001/solver.prototxt"
 cd -
